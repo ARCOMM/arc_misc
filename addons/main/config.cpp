78c5d57eb6807f3d0f2fc20256c8bd70a6a53cd8
Freddo3000 <f.falk@protonmail.com>
2020-02-29 14:13:19 +0100

    Add main component

---
+++ addons/main/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = "ARC Misc: Main";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "cba_main",
+			"A3_Data_F_Enoch_Loadorder",
+			"A3_Data_F_Mod_Loadorder"
+        };
+        author = "ARCOMM";
+        authors[] = {};
+        url = "https://github.com/ARCOMM/arc_misc";
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/main/script_macros.hpp
@@ -0,0 +1,2 @@
+#include "script_mod.hpp"
+#include "\x\cba\addons\main\script_macros_common.hpp"
