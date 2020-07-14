49611ded0e85ff012d9118cff446af8792d64696
Freddo3000 <f.falk@protonmail.com>
2020-07-14 20:40:18 +0200

    Move unit/vehicle init to function
    Makes it so that files do not have to be loaded every time a new unit is created.

--- addons/difficulty/CfgEventhandlers.hpp
+++ addons/difficulty/CfgEventhandlers.hpp
@@ -4,15 +4,21 @@ class Extended_PreInit_EventHandlers {
     };
 };
 
+class Extended_PreStart_EventHandlers {
+	class ADDON {
+		init = QUOTE(call COMPILE_FILE(XEH_preStart));
+	};
+};
+
 class Extended_Init_EventHandlers {
 	class CAManBase {
 		class ADDON {
-			init = QUOTE(call COMPILE_FILE(XEH_unitInit));
+			init = QUOTE(call FUNC(unitInit));
 		};
 	};
 	class LandVehicle {
 		class ADDON {
-			init = QUOTE(call COMPILE_FILE(XEH_vehicleInit));
+			init = QUOTE(call FUNC(vehicleInit));
 		};
 	};
 };
