f3cab0d43153f339c420385429a60ef413441c7a
Freddo <f.falk@protonmail.com>
2020-08-15 21:17:43 +0200

    Add additional adminmenu users
    * @BorderKeeper 
    * @Freddo3000

--- addons/tmf_loadouts/tmf_adminMenu_authorized_players.hpp
+++ addons/tmf_loadouts/tmf_adminMenu_authorized_players.hpp
@@ -11,4 +11,10 @@ class TMF_adminMenu_authorized_players {
 	class Draco194 {
 		uid = "76561198018898971";
 	};
+	class Borderkeeper {
+		uid = "76561198041490339";
+	};
+	class Freddo3000 {
+		uid = "76561198095500326";
+	};
 };
