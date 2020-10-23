9b69ece5ed597dd1f5f0d61e4391c6bf7a137e65
Freddo <f.falk@protonmail.com>
2020-10-23 12:26:38 +0200

    Hide duplicate weapons, ammo, add AK12 545 (#70)

---
+++ addons/weapons/CfgMagazines.hpp
@@ -0,0 +1,26 @@
+class CfgMagazines {
+	class CA_Magazine;
+	HIDE(150Rnd_762x51_Box,CA_Magazine);
+	HIDE(150Rnd_762x51_Box_Tracer,150Rnd_762x51_Box);
+	HIDE(150Rnd_762x54_Box,150Rnd_762x51_Box);
+	HIDE(150Rnd_762x54_Box_Tracer,150Rnd_762x54_Box);
+	HIDE(ACE_150Rnd_762x54_Box_red,150Rnd_762x54_Box);
+	HIDE(ACE_150Rnd_762x54_Box_yellow,150Rnd_762x54_Box);
+	HIDE(ACE_150Rnd_762x54_Box_tracer_red,150Rnd_762x54_Box);
+	HIDE(ACE_150Rnd_762x54_Box_tracer_yellow,150Rnd_762x54_Box);
+
+	HIDE(200Rnd_556x45_Box_F,CA_Magazine);
+	HIDE(200Rnd_556x45_Box_Red_F,200Rnd_556x45_Box_F);
+	HIDE(200Rnd_556x45_Box_Tracer_F,200Rnd_556x45_Box_F);
+	HIDE(200Rnd_556x45_Box_Tracer_Red_F,200Rnd_556x45_Box_Tracer_F);
+	HIDE(ACE_200Rnd_556x45_Box_green,200Rnd_556x45_Box_F);
+	HIDE(ACE_200Rnd_556x45_Box_tracer_green,200Rnd_556x45_Box_Tracer_F);
+
+	class CA_LauncherMagazine;
+	HIDE(MRAWS_HEAT_F,CA_LauncherMagazine);
+	HIDE(MRAWS_HEAT55_F,MRAWS_HEAT_F);
+	HIDE(MRAWS_HE_F,MRAWS_HEAT_F);
+
+	class RPG32_F;
+	HIDE(RPG7_F,RPG32_F);
+};
--- addons/weapons/script_component.hpp
+++ addons/weapons/script_component.hpp
@@ -1,2 +1,84 @@
 #define COMPONENT weapons
 #include "\x\arc_misc\addons\main\script_macros.hpp"
+
+#define HIDE(x,y) class x: y {scope = 1; scopeArsenal = 0;}
+
+#define AK74_MAGS {                                    \
+	"CUP_30Rnd_545x39_AK74_plum_M",                    \
+	"CUP_30Rnd_545x39_AK_M",                           \
+	"CUP_30Rnd_Subsonic_545x39_AK_M",                  \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK_M",          \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK_M",            \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK_M",          \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK_M",         \
+	"CUP_30Rnd_Subsonic_545x39_AK74_plum_M",           \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK74_plum_M",   \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK74_plum_M",     \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK74_plum_M",   \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK74_plum_M",  \
+	"CUP_30Rnd_545x39_AK74M_M",                        \
+	"CUP_30Rnd_Subsonic_545x39_AK74M_M",               \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK74M_M",       \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK74M_M",         \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK74M_M",       \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK74M_M",      \
+	"CUP_30Rnd_545x39_AK74M_camo_M",                   \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK_camo_M",     \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK_camo_M",       \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK_camo_M",     \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK_camo_M",    \
+	"CUP_30Rnd_545x39_AK74M_desert_M",                 \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK_desert_M",   \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK_desert_M",     \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK_desert_M",   \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK_desert_M",  \
+	"CUP_45Rnd_TE4_LRT4_Green_Tracer_545x39_RPK_M",    \
+	"CUP_45Rnd_TE4_LRT4_Green_Tracer_545x39_RPK74M_M", \
+	"CUP_60Rnd_545x39_AK74M_M",                        \
+	"CUP_60Rnd_TE1_Green_Tracer_545x39_AK74M_M",       \
+	"CUP_60Rnd_TE1_Red_Tracer_545x39_AK74M_M",         \
+	"CUP_60Rnd_TE1_White_Tracer_545x39_AK74M_M",       \
+	"CUP_60Rnd_TE1_Yellow_Tracer_545x39_AK74M_M",      \
+	"CUP_20Rnd_545x39_AKSU_M",                         \
+	"CUP_20Rnd_Subsonic_545x39_AKSU_M"                 \
+}
+
+#define RPK74_MAGS {                                   \
+	"CUP_60Rnd_545x39_AK74M_M",                        \
+	"CUP_30Rnd_545x39_AK74_plum_M",                    \
+	"CUP_30Rnd_545x39_AK_M",                           \
+	"CUP_30Rnd_Subsonic_545x39_AK_M",                  \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK_M",          \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK_M",            \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK_M",          \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK_M",         \
+	"CUP_30Rnd_Subsonic_545x39_AK74_plum_M",           \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK74_plum_M",   \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK74_plum_M",     \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK74_plum_M",   \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK74_plum_M",  \
+	"CUP_30Rnd_545x39_AK74M_M",                        \
+	"CUP_30Rnd_Subsonic_545x39_AK74M_M",               \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK74M_M",       \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK74M_M",         \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK74M_M",       \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK74M_M",      \
+	"CUP_30Rnd_545x39_AK74M_camo_M",                   \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK_camo_M",     \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK_camo_M",       \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK_camo_M",     \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK_camo_M",    \
+	"CUP_30Rnd_545x39_AK74M_desert_M",                 \
+	"CUP_30Rnd_TE1_Green_Tracer_545x39_AK_desert_M",   \
+	"CUP_30Rnd_TE1_Red_Tracer_545x39_AK_desert_M",     \
+	"CUP_30Rnd_TE1_White_Tracer_545x39_AK_desert_M",   \
+	"CUP_30Rnd_TE1_Yellow_Tracer_545x39_AK_desert_M",  \
+	"CUP_45Rnd_TE4_LRT4_Green_Tracer_545x39_RPK_M",    \
+	"CUP_45Rnd_TE4_LRT4_Green_Tracer_545x39_RPK74M_M", \
+	"CUP_60Rnd_TE1_Green_Tracer_545x39_AK74M_M",       \
+	"CUP_60Rnd_TE1_Red_Tracer_545x39_AK74M_M",         \
+	"CUP_60Rnd_TE1_White_Tracer_545x39_AK74M_M",       \
+	"CUP_60Rnd_TE1_Yellow_Tracer_545x39_AK74M_M",      \
+	"CUP_20Rnd_545x39_AKSU_M",                         \
+	"CUP_20Rnd_Subsonic_545x39_AKSU_M"                 \
+}
