4c0308f2177b8e224fbded1dd9487c10a6b4417d
Freddo <f.falk@protonmail.com>
2021-11-29 00:02:20 +0100

    Fix AAN (#155)
    Not sure why this line was commented out

--- addons/aan/config.cpp
+++ addons/aan/config.cpp
@@ -21,7 +21,7 @@ class CfgPatches
 
 #include "CfgEventHandlers.hpp"
 #include "CfgScriptPaths.hpp"
-//#include "\x\arc_misc\addons\tmf_loadouts\macros.inc"
+#include "\x\arc_misc\addons\tmf_loadouts\macros.inc"
 #include "CfgLoadouts.hpp"
 
 #include "\a3\ui_f\hpp\defineCommon.inc"
